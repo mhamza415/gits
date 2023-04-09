a7c82d40a88f60810fd8cd12e9531ff316dcc666
mhamza415 <mahamza415@gmail.com>
2023-04-09 15:58:43 +0500

    version 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function index() {
-  return <div>Hello world version 1</div>;
+  return <div>Hello world version 1 yes i am ammending all the changes.</div>;
 }
 
 export default index;
