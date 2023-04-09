5fa497bbb66f6b31b2240c74ed7654561967bfd4
mhamza415 <mahamza415@gmail.com>
2023-04-09 16:15:23 +0500

    version 2

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-import React from "react";
-
-function index() {
-  return <div>Hello world version 1 yes i am ammending all the changes.</div>;
-}
-
-export default index;
+console.log("index.js");
+console.log("version 1");
